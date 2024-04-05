943da75ec78c2b879dd82698e458d5e0e4b7ef72
Shidhanta95 <147026855+Shidhanta95@users.noreply.github.com>
2024-04-05 10:59:43 +0530

    Add files via upload

---
+++ data_extraction.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+
+
+def extract_data():
+
+    s3 = boto3.client('s3')
+    bucket_name = 'deeplearning-mlops-demo'
+    url = s3.generate_presigned_url(
+                    ClientMethod='get_object',
+                    Params={'Bucket': bucket_name, 'Key': 'construction dataset.zip'},
+                    ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
+                )
+    print(url)
+    return url
+
+extract_data()
